3d0aaa0e57c77bb23af4830a812bc6ea6591b1ce
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2025-08-04 18:06:45 -0400

    fix get

--- src/pynaviz/audiovideo/__init__.py
+++ src/pynaviz/audiovideo/__init__.py
@@ -1,3 +1,4 @@
 from .video_plot import PlotTsdTensor, PlotVideo
+from .audio_handling import AudioHandler
 
-__all__ = ["PlotVideo", "PlotTsdTensor"]
+__all__ = ["PlotVideo", "PlotTsdTensor", "AudioHandler"]
