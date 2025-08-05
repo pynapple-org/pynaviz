d0d7f6c999d2e998e81268c611a7043200501f0f
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2025-08-05 09:49:29 -0400

    started a base audiovideo class

--- src/pynaviz/audiovideo/__init__.py
+++ src/pynaviz/audiovideo/__init__.py
@@ -1,4 +1,4 @@
-from .video_plot import PlotTsdTensor, PlotVideo
 from .audio_handling import AudioHandler
+from .video_plot import PlotTsdTensor, PlotVideo
 
 __all__ = ["PlotVideo", "PlotTsdTensor", "AudioHandler"]
