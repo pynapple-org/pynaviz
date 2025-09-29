2027e37911081fc95891415fa1ed5826d9f0f878
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2025-09-29 16:21:16 -0400

    fix  import

--- tests/_test_widget_menu.py
+++ tests/_test_widget_menu.py
@@ -3,9 +3,9 @@ from collections import OrderedDict
 from unittest.mock import MagicMock
 
 import pytest
+from pynaviz.qt.widget_list_selection import ChannelListModel
 from PyQt6.QtWidgets import QApplication, QComboBox, QDoubleSpinBox
 
-from pynaviz.qt.qt_item_models import ChannelListModel
 from pynaviz.qt.widget_menu import ChannelList, DropdownDialog, MenuWidget, widget_factory
 
 
