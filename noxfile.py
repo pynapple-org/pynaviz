c37455604ea8ef74798bc75646622c6bff463f67
BalzaniEdoardo <edoardo.balzani87@gmail.com>
2025-09-30 09:53:09 -0400

    simplified ci

--- noxfile.py
+++ noxfile.py
@@ -3,15 +3,17 @@ import pathlib
 import nox
 
 
-@nox.session(name="linters")
+@nox.session(name="linters", reuse_venv=True)
 def linters(session):
     """Run linters"""
+    session.install("-e", ".[dev]")
     session.run("ruff", "check", "src", "--ignore", "D")
     session.run("ruff", "check", "tests", "--ignore", "D")
 
-@nox.session(name="linters-fix")
+@nox.session(name="linters-fix", reuse_venv=True)
 def linters_fix(session):
     """Run linters and auto-fix issues"""
+    session.install("-e", ".[dev]")
     session.run("ruff", "check", "src", "--ignore", "D", "--fix")
     session.run("ruff", "check", "tests", "--ignore", "D", "--fix")
 
